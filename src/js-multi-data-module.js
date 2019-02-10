98330841827987cfa541a4a511e68e52cdc0aa80
yama-dev <tatsuya.yamamoto69@gmail.com>
2019-02-10 19:02:43 +0900

    modify: 各オプションの最適化. add: On.Update callback-function. adjust: array sort setting.

--- src/js-multi-data-module.js
+++ src/js-multi-data-module.js
@@ -1,3 +1,4 @@
+/*eslint no-console: "off"*/
 
 import Promise from 'es6-promise';
 
@@ -5,29 +6,43 @@ export default class MULTI_DATA_MODULE {
 
   constructor(options={}){
 
+    let configDefault = {
+      data_type     : 'jsonp',
+      data_list     : null,
+      order         : 'up', // 'down'
+      orderProperty : 'date',
+      filter        : false,
+      jsonpCallback : 'callback',
+      fetch_timeout : 10000
+    };
+
+    if(!options.data_list || !Array.isArray(options.data_list)){
+      try {
+        throw new Error('Not config "data_list"');
+      } catch (e) {
+        console.log(e.name + ': ' + e.message);
+      }
+      return false;
+    }
+
+    // Merge Config Settings.
+    this.Config = Object.assign(configDefault, options);
+
     // Set Version.
     this.Version = process.env.VERSION;
 
-    // Set config, options.
-    this.Config = {
-      elem      : options.elem||null,
-      data_type : options.data_type||null,
-      data_list : options.data_list||null,
-      order     : options.order||'up', // 'down'
-      jsonpCallback : options.jsonpCallback||'callback',
-      fetch_timeout: 10000
-    }
-
     // Data obj.
     this.DataFix = [];
+    this.DataList = {};
 
     // Set callback functions.
     if(!options.on){
-      options.on = {}
+      options.on = {};
     }
-    this.on = {
-      Complete : options.on.Complete||'',
-    }
+    this.On = {
+      Update   : options.on.Update||'',
+      Complete : options.on.Complete||''
+    };
 
     // For Jsonp data.
     if(this.Config.data_type === 'jsonp'){
@@ -56,27 +71,36 @@ export default class MULTI_DATA_MODULE {
         window.callback = (response)=>{
           resolve(response);
         };
-        setTimeout(()=>{ reject('error') }, this.Config.fetch_timeout);
+        setTimeout(()=>{ reject('error'); }, this.Config.fetch_timeout);
       });
 
       promise
         .then((data)=>{
           // Success.
+          let _data = data;
 
-          let _data = eval('data' +'.'+ dataAry[count].hierarchy);
+          // Select data hierarchy.
+          dataAry[count].hierarchy.split('.').map((item)=>{
+            _data = _data[item];
+          });
 
-          let _func = null;
-          if(dataAry[count].customFunction) _func = dataAry[count].customFunction;
+          // Set function to format data.
+          if(dataAry[count].customFunction){
+            _data = this.CreateData(_data, dataAry[count].customFunction);
+          }
 
-          let _data_array = _data;
-          if(_func) _data_array = this.CreateData(_data, _func);
-          if(_data_array) this.DataFix = this.DataFix.concat(_data_array);
+          if(_data){
+            this.DataFix = this.DataFix.concat(_data);
+            this.DataList[count] = _data;
+          }
+
+          this.OnUpdate(this.DataList[count]);
 
           count++;
           if(count < countMax){
             getDataFunc();
           } else {
-            this.OnComplete();
+            this.FormatData();
           }
 
         })
@@ -84,7 +108,7 @@ export default class MULTI_DATA_MODULE {
           // Error.
           console.log(err);
         });
-    }
+    };
 
     getDataFunc();
 
@@ -94,14 +118,29 @@ export default class MULTI_DATA_MODULE {
     return func(data);
   }
 
-  OnComplete(){
+  FormatData(){
+    let _that = this;
+
+    if(!Array.isArray(this.DataFix)){
+      this.OnComplete();
+      return false;
+    }
+
+    if(this.Config.filter){
+      let _dataFix = [];
+      this.DataFix.map((item)=>{
+        if(item[_that.Config.orderProperty]) _dataFix.push(item);
+      });
+      this.DataFix = _dataFix;
+    }
 
     // Sort Data.
     if(this.Config.order === 'up'){
       this.DataFix.sort(function(a,b){
-        if(!a.date || !b.date ) return 0;
-        a = new Date(a.date.replace(/\./g,'/'));
-        b = new Date(b.date.replace(/\./g,'/'));
+        if(!a[_that.Config.orderProperty]) return 1;
+        if(!b[_that.Config.orderProperty] ) return -1;
+        a = new Date(a[_that.Config.orderProperty].replace(/\./g,'/'));
+        b = new Date(b[_that.Config.orderProperty].replace(/\./g,'/'));
         if(a < b) return -1;
         if(a > b) return 1;
         return 0;
@@ -109,17 +148,30 @@ export default class MULTI_DATA_MODULE {
     }
     if(this.Config.order === 'down'){
       this.DataFix.sort(function(a,b){
-        if(!a.date || !b.date ) return 0;
-        a = new Date(a.date.replace(/\./g,'/'));
-        b = new Date(b.date.replace(/\./g,'/'));
+        if(!a[_that.Config.orderProperty]) return 1;
+        if(!b[_that.Config.orderProperty] ) return -1;
+        a = new Date(a[_that.Config.orderProperty].replace(/\./g,'/'));
+        b = new Date(b[_that.Config.orderProperty].replace(/\./g,'/'));
         if(a < b) return 1;
         if(a > b) return -1;
         return 0;
       });
     }
+    this.OnComplete();
+  }
 
-    if(this.on.Complete && typeof(this.on.Complete) === 'function') this.on.Complete(this.DataFix);
+  OnUpdate(data){
+    // Callback function.
+    if(this.On.Update && typeof(this.On.Update) === 'function'){
+      this.On.Update(data);
+    }
+  }
 
+  OnComplete(){
+    // Callback function.
+    if(this.On.Complete && typeof(this.On.Complete) === 'function'){
+      this.On.Complete(this.DataFix, this.DataList);
+    }
   }
 
 }
